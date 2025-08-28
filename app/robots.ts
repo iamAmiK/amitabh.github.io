d561ca4c2ee7fae47f092ac79946ce717126cc83
iamAmiK <150634703+iamAmiK@users.noreply.github.com>
2025-08-28 22:58:41 +1000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,9 @@
 import type { MetadataRoute } from "next";
 import { siteMetadata } from "@/app/data/siteMetadata";
 
+export const dynamic = "force-static";
+export const revalidate = 86400; // 1 day
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
