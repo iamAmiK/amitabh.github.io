a8014554aed515d6131d080b01e310f2f8d86061
iamAmiK <150634703+iamAmiK@users.noreply.github.com>
2025-09-06 00:34:30 +1000

    Update SocialPill.tsx

--- app/components/SocialPill.tsx
+++ app/components/SocialPill.tsx
@@ -1,5 +1,4 @@
 import { siteMetadata } from "app/data/siteMetadata";
-
 export function SocialPill() {
   return (
     <div 
@@ -10,7 +9,7 @@ export function SocialPill() {
         <img
           src="/blog/email.svg"
           alt="Email"
-          className="h-8 w-8 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
+          className="h-10 w-10 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
         />
         <span className="sr-only">Email</span>
       </a>
@@ -18,7 +17,7 @@ export function SocialPill() {
         <img
           src="/blog/github.svg"
           alt="GitHub"
-          className="h-8 w-8 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
+          className="h-10 w-10 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
         />
         <span className="sr-only">GitHub</span>
       </a>
@@ -26,7 +25,7 @@ export function SocialPill() {
         <img
           src="/blog/youtube.svg"
           alt="YouTube"
-          className="h-8 w-8 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
+          className="h-10 w-10 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
         />
         <span className="sr-only">YouTube</span>
       </a>
@@ -38,7 +37,7 @@ export function SocialPill() {
         <img
           src="/blog/linkedin.svg"
           alt="LinkedIn"
-          className="h-8 w-8 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
+          className="h-10 w-10 sm:h-6 sm:w-6 text-gray-400 hover:text-gray-300 transition-colors"
         />
         <span className="sr-only">LinkedIn</span>
       </a>
